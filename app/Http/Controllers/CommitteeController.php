f7fa34a16466df1cd1bad17311e724bfbff28be0
Ismail Elkhalili <ismail.elkalili@gmail.com>
2023-01-21 12:07:28 +0200

    Make dashbord file and committee

--- app/Http/Controllers/CommitteeController.php
+++ app/Http/Controllers/CommitteeController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CommitteeController extends Controller
 {
-    //
+    public function index()
+    {
+       $itemCommittee = DB::table('committees')->get();
+        return view('dashborard_layout.dashborard_main');
+    }
+
+    public function create()
+    {
+    
+    }
+
+    public function store(Request $request)
+    {
+    
+    }
+
+
+    public function edit($committeeID)
+    {
+
+    }
+  
+    public function update(Request $request, $committeeID)
+    {
+
+
+    }
+
+    public function destroy($bookID)
+    {
+
+    }
+
+
 }
